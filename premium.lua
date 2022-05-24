fb11c0e015928f47f75dd45d9687de80f55360b5
Notaskid <53245729+Notaskid@users.noreply.github.com>
2022-05-23 19:08:32 -0700

    Create premium.lua

---
+++ premium.lua
@@ -0,0 +1,13 @@
+--// put the player's user id in this format "[userid] = true,"
+
+prem = {
+    [609282484] = true,--Nuts
+    [478777] = true,--exodus
+    [820633508] = true,--jaay
+    [1351100055] = true,--mvnny
+    [2278738051] = true,--ali
+    [2200842965] = true,--chorriko
+    [3186573780] = true,--nuts alt
+}
+
+return prem
