dc21b6e8fcd21facb944268ade9f52b45bab7335
Notaskid <53245729+Notaskid@users.noreply.github.com>
2022-05-23 19:06:18 -0700

    Create acbypass.lua

---
+++ acbypass.lua
@@ -0,0 +1,25 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local tablefind = table.find
+local MainEvent = ReplicatedStorage.MainEvent
+local Flags = {
+    "CHECKER_1",
+    "TeleportDetect",
+    "OneMoreTime"
+}
+local __namecall
+__namecall = hookmetamethod(game, "__namecall", function(...)
+    local args = {...}
+    local self = args[1]
+    local method = getnamecallmethod()
+    local caller = getcallingscript()
+    if (method == "FireServer" and self == MainEvent and tablefind(Flags, args[2])) then
+        print("ac event fired | discord.gg/fraudz")
+        return
+    end
+    if (not checkcaller() and getfenv(2).crash) then
+        hookfunction(getfenv(2).crash, function()
+            warn("crash attempt | discord.gg/fraudz") 
+        end)
+    end
+    return __namecall(...)
+end)
